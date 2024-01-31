52c40754766a305961a9eee54d6670377ebd5030
Macharia Marigi <machariamarigi@gmail.com>
2024-01-31 17:16:49 +0300

    add the Layout for the marketing section

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <main className="h-full pt-40">{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
