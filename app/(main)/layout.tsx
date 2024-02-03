65fe5bfe4dcdb4bc8da556cc22d7923e5dc33f9d
Macharia Marigi <machariamarigi@gmail.com>
2024-02-03 12:33:12 +0300

    add navigation component which will be the sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@
 import { Spinner } from "@/components/ui/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -19,6 +20,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   }
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
       <main className="flex-1 h-full overflow-y-auto">{children}</main>
     </div>
   );
