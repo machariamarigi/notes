0a6e21abd87027270a6543f41067820df576fc9d
Macharia Marigi <machariamarigi@gmail.com>
2024-02-05 22:45:35 +0300

    add function to create notes from the frontend

--- app/(main)/(routes)/dashboard/page.tsx
+++ app/(main)/(routes)/dashboard/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
+import { toast } from "sonner";
 
 const DashboardPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+    toast.promise(promise, {
+      loading: "Creating note...",
+      success: "Note created!",
+      error: "Failed to create note",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -24,7 +38,7 @@ const DashboardPage = () => {
         className="hidden dark:block"
       />
       <h2>Welcome to {user?.firstName}&apos;s Notes</h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a Note
       </Button>
