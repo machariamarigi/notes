c8b6767f8df43f4e4661d0f799689cbc94d4414b
Macharia Marigi <machariamarigi@gmail.com>
2024-02-05 22:35:09 +0300

    add method to create a document with the associeted user and optional parent document

---
+++ convex/documents.ts
@@ -0,0 +1,28 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const create = mutation({
+  args: {
+    title: v.string(),
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const document = await ctx.db.insert("documents", {
+      title: args.title,
+      userId,
+      isArchived: false,
+      parentDocument: args.parentDocument,
+      isPublished: false,
+    });
+
+    return document;
+  },
+});
