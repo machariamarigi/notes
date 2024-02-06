b19b3fda85e84b95c0dde81a50d15211ed924a4a
Macharia Marigi <machariamarigi@gmail.com>
2024-02-06 17:02:25 +0300

    add a DocumentList component to display list of Items

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -16,6 +16,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -145,7 +146,9 @@ export const Navigation = () => {
           <Item label="Settings" icon={Settings} onClick={() => {}} />
           <Item label="New Page" icon={PlusCircle} onClick={handleCreate} />
         </div>
-        <div className="mt-4"></div>
+        <div className="mt-4">
+          <DocumentList />
+        </div>
         <div
           className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
           onMouseDown={handleMouseDown}
