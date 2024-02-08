155464f8f7716ad064b79e6e5d2fcae3dce6a03f
Macharia Marigi <machariamarigi@gmail.com>
2024-02-08 21:07:44 +0300

    add method to remove documents from convex

--- convex/documents.ts
+++ convex/documents.ts
@@ -197,3 +197,44 @@ export const restore = mutation({
     return document;
   },
 });
+
+const remove = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Document not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Not authorized");
+    }
+
+    const recursiveRemove = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) => {
+          return q.eq("userId", userId).eq("parentDocument", documentId);
+        })
+        .collect();
+
+      for (const child of children) {
+        await recursiveRemove(child._id);
+      }
+
+      await ctx.db.delete(documentId);
+    };
+
+    recursiveRemove(args.id);
+  },
+});
+
