3a3bc0101dea07bcf2f4fad1fc8be3511e168b0e
Macharia Marigi <machariamarigi@gmail.com>
2024-02-08 20:37:44 +0300

    add method to return archived documents from convex

--- convex/documents.ts
+++ convex/documents.ts
@@ -43,6 +43,30 @@ export const getSidebar = query({
     return documents;
   },
 });
+
+export const getTrash = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user_parent", (q) => {
+        return q.eq("userId", userId);
+      })
+      .filter((q) => q.eq(q.field("isArchived"), true))
+      .order("desc")
+      .collect();
+
+    return documents;
+  },
+});
+
 export const create = mutation({
   args: {
     title: v.string(),
