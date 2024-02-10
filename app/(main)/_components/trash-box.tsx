90c70683a74cbe13cfbf064856040d2649308702
Macharia Marigi <machariamarigi@gmail.com>
2024-02-10 22:45:24 +0300

    delete notes from the frontend

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Input } from "@/components/ui/input";
 import { Spinner } from "@/components/ui/spinner";
 import { api } from "@/convex/_generated/api";
@@ -42,12 +43,12 @@ export const TrashBox = () => {
   };
 
   const onRemove = async (id: Id<"documents">) => {
-    const promise = restore({ id });
+    const promise = remove({ id });
 
     toast.promise(promise, {
       loading: "Deleting Note...",
-      success: "Note Restored",
-      error: "Failed to restore Note",
+      success: "Note deleted",
+      error: "Failed to delete Note",
     });
 
     if (params.documentId === id) {
@@ -95,12 +96,14 @@ export const TrashBox = () => {
               >
                 <Undo className="h-4 w-4 text-muted-foreground" />
               </div>
-              <div
-                role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200"
-              >
-                <Trash className="h-4 w-4 text-muted-foreground" />
-              </div>
+              <ConfirmModal onConfirm={() => onRemove(doc._id)}>
+                <div
+                  role="button"
+                  className="rounded-sm p-2 hover:bg-neutral-200"
+                >
+                  <Trash className="h-4 w-4 text-muted-foreground" />
+                </div>
+              </ConfirmModal>
             </div>
           </div>
         ))}
