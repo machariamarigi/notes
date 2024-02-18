3d4d32b6de4cb8e8e3653804cb645f8b8abd94d8
Macharia Marigi <machariamarigi@gmail.com>
2024-02-18 16:58:47 +0300

    use Banner component to delete notes

--- app/(main)/_components/banner.tsx
+++ app/(main)/_components/banner.tsx
@@ -1,11 +1,65 @@
 "use client";
 
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface BannerProps {
   documentId: Id<"documents">;
 }
 
 export const Banner = ({ documentId }: BannerProps) => {
-  return <div>Banner</div>;
+  const router = useRouter();
+
+  const remove = useMutation(api.documents.remove);
+  const restore = useMutation(api.documents.restore);
+
+  const onRemove = () => {
+    const promise = remove({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Removing note...",
+      success: "Note removed",
+      error: "Failed to remove note",
+    });
+
+    router.push("/documents");
+  };
+
+  const onRestore = () => {
+    const promise = restore({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Restoring note...",
+      success: "Note restored",
+      error: "Failed to restore note",
+    });
+  };
+
+  return (
+    <div className="w-full bg-rose-500 text-center text-sm p-2 text-white flex items-center gap-x-2 justify-center">
+      <p>This Page is archived.</p>
+      <Button
+        size="sm"
+        onClick={onRestore}
+        variant="outline"
+        className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 h-auto font-normal"
+      >
+        Restore Page
+      </Button>
+      <ConfirmModal onConfirm={onRemove}>
+        <Button
+          size="sm"
+          variant="outline"
+          className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 h-auto font-normal"
+        >
+          Delete Forever
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
 };
