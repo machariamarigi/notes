5fd310eac5359c45647a1606a0f5968a0e53494c
Macharia Marigi <machariamarigi@gmail.com>
2024-02-25 00:11:42 +0300

    add ability to upload image for notes

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -2,9 +2,44 @@
 
 import { useCoverImage } from "@/hooks/use-cover-image";
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
+import { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { SingleImageDropzone } from "../singe-image-dropzone";
 
 export const CoverImageModal = () => {
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+  const update = useMutation(api.documents.update);
+  const params = useParams();
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({ file });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+
+      onClose();
+    }
+  };
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
@@ -12,7 +47,12 @@ export const CoverImageModal = () => {
         <DialogHeader>
           <h2 className="text-center text-lg font-semibold">Cover Image</h2>
         </DialogHeader>
-        <div>TODO: Upload Image</div>
+        <SingleImageDropzone
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
       </DialogContent>
     </Dialog>
   );
