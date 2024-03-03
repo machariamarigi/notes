d06dc25da4fd1b6d7c68582c585f5320374a35d2
Macharia Marigi <machariamarigi@gmail.com>
2024-03-03 16:28:25 +0300

    use the block note editor to edit note and add images

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { useTheme } from "next-themes";
+import { BlockNoteEditor } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+import { useEdgeStore } from "@/lib/edgestore";
+
 interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
@@ -7,9 +13,31 @@ interface EditorProps {
 }
 
 export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpoad = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({ file });
+    return response.url;
+  };
+
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+    uploadFile: handleUpoad,
+  });
+
   return (
     <div>
-      Editor
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
     </div>
   );
-}
+};
+
+export default Editor;
